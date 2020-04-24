287861e757cdf171d3520f467ebe67e1ab86226b
Shrey <svaity@stevens.edu>
2020-04-24 19:32:39 -0400

    Added Orderers name

--- src/users/views.py
+++ src/users/views.py
@@ -7,8 +7,10 @@ from .models import Profile
 def my_profile(request):
     my_user_profile = Profile.objects.filter(user=request.user).first()
     my_orders = Order.objects.filter(is_ordered=True, owner=my_user_profile)
+    my_user = Profile.objects.filter(user=request.user)
     context = {
-        'my_orders': my_orders
+        'my_orders': my_orders,
+        'my_user': my_user
     }
 
     return render(request, "profile.html", context)
